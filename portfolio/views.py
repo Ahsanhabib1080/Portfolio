f18532356fd61d89847e93ccf0a589ab4aa04717
Ahsanhabib1080 <Ahsanhabib1080@users.noreply.github.com>
2020-09-25 21:23:09 +0600

    Portfolio Experimental V1.0

---
+++ portfolio/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Project
+
+# render will always return from insider templates folder
+# You have to keep your .html file in a separate folder under 'templates' folder
+# { 'projects': projects}) this means passing project perameter (2nd project) and reference first project perameter for the desired HTML file
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/menu.html', {'projects': projects})
