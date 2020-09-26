02f5b6cb55f0a0f2bf7804d7b6da4e9d86de217b
Ahsanhabib1080 <Ahsanhabib1080@users.noreply.github.com>
2020-09-26 11:22:33 +0600

    Portfolio V1.1

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Project
 
+class MyModelAdmin(admin.ModelAdmin):
+
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
 admin.site.register(Project)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+# Receive the pre_delete signal and delete the file associated with the model instance.
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
+from django.contrib.admin.actions import delete_selected
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
@@ -8,3 +12,7 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        super().delete(*args, **kwargs)
